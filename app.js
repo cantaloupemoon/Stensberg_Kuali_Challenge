c8d301f0b29d21e24b7a68137f6c5f53a3e9647b
Shawn Stensberg <shawn.stensberg@gmail.com>
2021-08-27 21:50:50 -0700

    Update app.js
    move notes add into main app function

--- app.js
+++ app.js
@@ -1,21 +1,12 @@
 const CSVToJSON = require('csvtojson');
 let csvFilePath = 'courses.csv';
 
-// function that converts CSV to JSON file
-async function convertCSVtoJSON(csv) {
+async function mainAppRun(csv) {
     const courses = await CSVToJSON().fromFile(csv)
-    return courses;
+    for (var i = 0; i < courses.length; i++) {
+        courses[i]["Notes"] = "Submitted by Shawn Stensberg";
+    }
+    console.log(courses);
 }
 
-// function that addes notes to returned JSON
-function addNotesToCourses(csvFilePath) {
-    convertCSVtoJSON(csvFilePath)
-        .then(courses => {
-            for (let i = 0; i < courses.length; i++) {
-                courses[i]["Notes"] = "Submitted by Shawn Stensberg"
-            }
-            console.log(courses)
-        })
-}
-
-addNotesToCourses(csvFilePath);
+mainAppRun(csvFilePath)
