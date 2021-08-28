c064be8acd4b3ca30267567e22c2a28253e033df
Shawn Stensberg <shawn.stensberg@gmail.com>
2021-08-27 20:26:24 -0700

    Removed object Course and courses.json
    Decided to make several small functions for app instead of object Course methods. Also no longer generating a json file - just saving courses json to a variable.

--- app.js
+++ app.js
@@ -1,25 +1,17 @@
-//initial parser test to parse CSV to JSON and create JSON file output
-
 const CSVToJSON = require('csvtojson');
 const csvFilePath = 'courses.csv';
 const fs = require('fs');
 
-function Course() {
-    this.getJSON = function(csv) {
-        CSVToJSON().fromFile(csv)
-            .then(courses => {
-                fs.writeFile('courses.json', JSON.stringify(courses, null, 4), (err) => {
-                    if (err) {
-                        throw err;
-                    }
-                    console.log("JSON array is saved.");
-                });
-            }).catch(err => {
-                console.log(err);
-            });
-    }
+// function that converts CSV to JSON file
+let getJson = function(csv) {
+    CSVToJSON().fromFile(csv)
+    .then(courses => {
+        let coursesJSON = courses;
+        console.log(coursesJSON);
+        return coursesJSON;
+    }).catch(err => {
+        console.log(err);
+    });
 }
 
-
-let courses = new Course();
-courses.getJSON(csvFilePath);
+getJson(csvFilePath);
