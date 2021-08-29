c7efd39f5d968b007a967a0335c23f6a91917c92
Shawn Stensberg <shawn.stensberg@gmail.com>
2021-08-28 19:00:55 -0700

    Update to app.js adding initial API connection
    Attempting to set up API connection but getting error "Invalid character in header content ["Authorization"]"

--- app.js
+++ app.js
@@ -3,11 +3,26 @@ const axios = require('axios');
 let csvFilePath = 'courses.csv';
 let api_config = require('./api_config.json');
 
-async function mainAppRun(csv) {
-    const courses = await CSVToJSON().fromFile(csv)
-    for (var i = 0; i < courses.length; i++) {
-        courses[i]["Notes"] = "Submitted by Shawn Stensberg";
+// async function mainAppRun(csv) {
+//     const courses = await CSVToJSON().fromFile(csv)
+//     for (var i = 0; i < courses.length; i++) {
+//         courses[i]["Notes"] = "Submitted by Shawn Stensberg";
+//     }
+//     console.log(courses);
+// }
+// mainAppRun(csvFilePath)
+
+
+const connectionConfig = {
+    headers: {
+        'Authorization': api_config.API_KEY
     }
-    console.log(courses);
 }
-mainAppRun(csvFilePath)
+
+axios.get(api_config.BASE_URL + api_config.CAMPUSES_OPTIONS_URI, connectionConfig)
+    .then((response) => {
+        console.log(response.data);
+    })
+    .catch((err) => {
+        console.log(err)
+    })
