cd089cb8965a84966081278db3ff8a0931e5bab3
Shawn Stensberg <shawn.stensberg@gmail.com>
2021-08-30 00:12:25 -0700

    Formatting on app.js
    Added pretty formatting for credits, status, notes, and date start

--- app.js
+++ app.js
@@ -2,41 +2,75 @@ const CSVToJSON = require('csvtojson');
 const axios = require('axios');
 let csvFilePath = 'courses.csv';
 const api_config = require('./api_config.json');
-const fetch = require('node-fetch')
 
 // main app function that converts CSV to JSON
 async function mainAppRun(csv) {
-    var courses = await CSVToJSON().fromFile(csv)
-    for (var i = 0; i < courses.length; i++) {
-        courses[i]["Notes"] = "Submitted by Shawn Stensberg";
-        if (courses[i].creditType == "fixed") {
-            delete courses['creditType'];
-            courses[i]["credits"] = {
-                "min": courses[i].creditsMin,
-                "max": courses[i].creditsMin
+    var rawCourses = await CSVToJSON().fromFile(csv)
+    console.log(rawCourses);
+    var formattedCourses = []
+    for (var i = 0; i < rawCourses.length; i++) {
+        var newCourse = {
+            "subjectCode": rawCourses[i].subjectCode,
+            "number": rawCourses[i].number,
+            "title": rawCourses[i].title,
+            "credits": {
+                "chosen": rawCourses[i].creditType,
+                "credits": {
+                    "min": "0",
+                    "max": "0"
+                },
+                "value": "0",
+            },
+            "status": "draft",
+            "dateStart": rawCourses[i].dateStart,
+            "groupFilter1": "",
+            "groupFilter2": "",
+            "campus": {},
+            "notes": "Shawn Stensberg says: Unicorns are Magnificent Beasts"
+        }
+        formattedCourses.push(newCourse);
+
+        if (rawCourses[i].creditType == "fixed") {
+            formattedCourses[i].credits.credits.min = rawCourses[i].creditsMin
+            formattedCourses[i].credits.credits.max = rawCourses[i].creditsMin
+            formattedCourses[i].credits.value = rawCourses[i].creditsMin
+
+        } else if (rawCourses[i].creditType == "multiple") {
+            delete formattedCourses[i].credits.min
+            formattedCourses[i].credits.credits.max = rawCourses[i].creditsMax
+            formattedCourses[i].credits.credits["min"] = rawCourses[i].creditsMin
+            formattedCourses[i].credits.value = [
+                rawCourses[i].creditsMin,
+                rawCourses[i].creditsMax
+            ]
+        } else {
+            formattedCourses[i].credits.credits.min = rawCourses[i].creditsMin
+            formattedCourses[i].credits.credits.max = rawCourses[i].creditsMax
+            formattedCourses[i].credits.value = {
+                "min": rawCourses[i].creditsMin,
+                "max": rawCourses[i].creditsMax
             }
-            courses[i]["value"] = courses[i].creditsMin
-            delete courses['creditsMin']
-            delete courses['creditsMax']
-        } else if (courses[i].creditType == "multiple") {
-            delete courses['creditType'];
-            courses[i]["credits"] = {
-                "max": courses[i].creditsMax,
-                "min": courses[i].creditsMin
-            }
-            courses[i]["value"] = [courses[i].creditsMin, courses[i].creditsMax]
-            delete courses['creditsMin']
-            delete courses['creditsMax']
+        }
+        if (rawCourses[i].dateStart.includes("Winter")) {
+            formattedCourses[i].dateStart = "2021-01-01"
+        } else if (rawCourses[i].dateStart.includes("Spring")) {
+            formattedCourses[i].dateStart = "2021-04-03"
+        } else if (rawCourses[i].dateStart.includes("Summer")) {
+            formattedCourses[i].dateStart = "2021-07-04"
+        } else {
+            formattedCourses[i].dateStart = "2021-10-04"
         }
     }
-    return courses;
+    // console.log(formattedCourses)
+    console.log(JSON.stringify(formattedCourses))
+    return rawCourses;
 }
 
 // executes mainAppRun to convert CSV to JSON and stores in variable I can use in the global scope
 let jsonCourses = mainAppRun(csvFilePath);
 jsonCourses.then(function(result){
     var courses = result;
-    console.log(courses);
+    // console.log(courses);
     // // configuration for Authorization header to use in API calls
     const connectionConfig = {
         headers: {
