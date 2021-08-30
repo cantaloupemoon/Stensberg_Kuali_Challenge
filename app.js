133dde5b7bfc32014d6d861d238de562dbe487f7
Shawn Stensberg <shawn.stensberg@gmail.com>
2021-08-29 20:50:43 -0700

    Added comments to app.js

--- app.js
+++ app.js
@@ -4,23 +4,29 @@ let csvFilePath = 'courses.csv';
 const api_config = require('./api_config.json');
 const fetch = require('node-fetch')
 
-// async function mainAppRun(csv) {
-//     const courses = await CSVToJSON().fromFile(csv)
-//     for (var i = 0; i < courses.length; i++) {
-//         courses[i]["Notes"] = "Submitted by Shawn Stensberg";
-//     }
-//     console.log(courses);
-// }
-// mainAppRun(csvFilePath)
+// main app function that converts CSV to JSON
+async function mainAppRun(csv) {
+    var courses = await CSVToJSON().fromFile(csv)
+    for (var i = 0; i < courses.length; i++) {
+        courses[i]["Notes"] = "Submitted by Shawn Stensberg";
+    }
+    return courses;
+}
 
-console.log(/\t\r\n/.test(api_config.API_KEY)); // test to determine if key has carriage returns, new lines, or tabs
+// executes mainAppRun to convert CSV to JSON and stores in variable I can use in the global scope
+let jsonCourses = mainAppRun(csvFilePath);
+jsonCourses.then(function(result){
+    console.log(result);
+})
 
+// configuration for Authorization header to use in API calls
 const connectionConfig = {
     headers: {
         'Authorization': "Bearer " + api_config.API_KEY
     }
 }
 
+// API get request to retrieve subject codes
 axios.get(api_config.BASE_URL + api_config.SUBJECTCODES_OPTIONS_URI, connectionConfig)
     .then((response) => {
         console.log(response.data);
